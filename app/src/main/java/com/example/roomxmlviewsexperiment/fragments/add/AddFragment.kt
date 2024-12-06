4bc4d053748ef625ec1b193c4746200ad6ed1f86
Natanael Alves Fernandes <natanael.alves.fernandes@gmail.com>
2024-12-06 17:49:50 -0300

    refactor: use viewbindings in fragments

--- app/src/main/java/com/example/roomxmlviewsexperiment/fragments/add/AddFragment.kt
+++ app/src/main/java/com/example/roomxmlviewsexperiment/fragments/add/AddFragment.kt
@@ -6,15 +6,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.roomxmlviewsexperiment.R
+import com.example.roomxmlviewsexperiment.databinding.FragmentAddBinding
 
 class AddFragment : Fragment() {
+
+    private lateinit var binding: FragmentAddBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add, container, false)
+    ): View {
+        binding = FragmentAddBinding.inflate(layoutInflater)
+
+        return binding.root
     }
 
 }
--- app/src/main/java/com/example/roomxmlviewsexperiment/fragments/list/ListFragment.kt
+++ app/src/main/java/com/example/roomxmlviewsexperiment/fragments/list/ListFragment.kt
@@ -1,28 +1,29 @@
 package com.example.roomxmlviewsexperiment.fragments.list
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.roomxmlviewsexperiment.R
-import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.example.roomxmlviewsexperiment.databinding.FragmentListBinding
 
 class ListFragment : Fragment() {
+
+    private lateinit var binding: FragmentListBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        val view = inflater.inflate(R.layout.fragment_list, container, false)
+    ): View {
+        binding = FragmentListBinding.inflate(layoutInflater)
 
-        view.findViewById<FloatingActionButton>(R.id.fabAdd).setOnClickListener {
+        binding.fabAdd.setOnClickListener() {
             findNavController().navigate(R.id.action_listFragment_to_addFragment)
         }
 
-        return view
+        return binding.root
     }
 
 }
