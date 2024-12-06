a1c646dc3b5cab9773b89e61de1cffab624ef8ae
Natanael Alves Fernandes <natanael.alves.fernandes@gmail.com>
2024-12-06 17:42:31 -0300

    feat: add list and add fragments

--- app/src/main/java/com/example/roomxmlviewsexperiment/MainActivity.kt
+++ app/src/main/java/com/example/roomxmlviewsexperiment/MainActivity.kt
@@ -5,6 +5,9 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +19,9 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        setupActionBarWithNavController(
+                supportFragmentManager.findFragmentById(R.id.fragmentContainerView)!!.findNavController()
+            )
     }
 }
