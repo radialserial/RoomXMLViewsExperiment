484c91502b55fd57cae671ce92fda7e3ffabbdb4
Natanael Alves Fernandes <natanael.alves.fernandes@gmail.com>
2024-12-06 18:14:26 -0300

    feat(AddFragment): add database functionality

--- app/src/main/java/com/example/roomxmlviewsexperiment/fragments/add/AddFragment.kt
+++ app/src/main/java/com/example/roomxmlviewsexperiment/fragments/add/AddFragment.kt
@@ -1,16 +1,22 @@
 package com.example.roomxmlviewsexperiment.fragments.add
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.example.roomxmlviewsexperiment.R
 import com.example.roomxmlviewsexperiment.databinding.FragmentAddBinding
+import com.example.roomxmlviewsexperiment.model.User
+import com.example.roomxmlviewsexperiment.viewmodel.UserViewModel
 
 class AddFragment : Fragment() {
 
     private lateinit var binding: FragmentAddBinding
+    private val userViewModel by viewModels<UserViewModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -18,7 +24,35 @@ class AddFragment : Fragment() {
     ): View {
         binding = FragmentAddBinding.inflate(layoutInflater)
 
+        binding.btAdd.setOnClickListener {
+            addDataToDatabase()
+        }
+
         return binding.root
+    }
+
+    private fun addDataToDatabase() {
+        val firstName = binding.etFirstName.text.toString()
+        val lastName = binding.etLastName.text.toString()
+        val age = binding.etAge.text.toString()
+
+        if (firstName.isNotEmpty() && lastName.isNotEmpty() && age.isNotEmpty()) {
+            userViewModel.addUser(
+                User(
+                    0,
+                    firstName,
+                    lastName,
+                    age.toInt()
+                )
+            )
+            Toast.makeText(requireContext(), "Successfully added user.", Toast.LENGTH_LONG).show()
+            findNavController().navigate(R.id.action_addFragment_to_listFragment)
+        } else {
+            Toast.makeText(requireContext(), "Please fill out all fields.", Toast.LENGTH_LONG)
+                .show()
+        }
+
+
     }
 
 }
