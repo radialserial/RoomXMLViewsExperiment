b9d367727d5ddea84e9be239c14ba8fe36bd0a0f
Natanael Alves Fernandes <natanael.alves.fernandes@gmail.com>
2024-12-06 18:13:28 -0300

    refactor(MainActivity): use viewbinding

--- app/src/main/java/com/example/roomxmlviewsexperiment/MainActivity.kt
+++ app/src/main/java/com/example/roomxmlviewsexperiment/MainActivity.kt
@@ -5,15 +5,21 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.roomxmlviewsexperiment.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -21,7 +27,8 @@ class MainActivity : AppCompatActivity() {
         }
 
         setupActionBarWithNavController(
-                supportFragmentManager.findFragmentById(R.id.fragmentContainerView)!!.findNavController()
-            )
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView)!!
+                .findNavController()
+        )
     }
 }
